79bf0bfbee8eecb8de9cf327623829be97e23eff
unknown <rohansp00@gmail.com>
2019-10-29 05:40:16 +0530

    basic skeleton

---
+++ src/pages/homePage/homePage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./homePage.scss";
+const homePage = () => {
+  return (
+    <div className="homepage">
+      <div className="categories">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Product 1</h1>
+            <span className="subtitle">View Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Product 1</h1>
+            <span className="subtitle">View Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Product 1</h1>
+            <span className="subtitle">View Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Product 1</h1>
+            <span className="subtitle">View Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Product 1</h1>
+            <span className="subtitle">View Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default homePage;
